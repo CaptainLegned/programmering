57d97fb041abfd5c78562fd207a788e0e351e73c
CaptainLegned <captainlegend150@gmail.com>
2026-01-08 17:12:00 +0000

    v1.2

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-print("Hello world!")
-print("ZZzzzzzzzzz")
-a = 1
-print(a)
+def spin_row():
+    pass
+
+def print_row():
+    pass
+
+def get_payput():
+    pass
+
+def main():
+    balance = 100
+    print("***********************")
+    print("welcome to aziz system")
+    print("***********************")  
+    print("***********************")
+
+
+
+
+balance = 100
+print(f"Current balance: ${balance}")
+
+bet = input("place your bet amoumt")
+
+if __name__ == '__main__':
+    main()
